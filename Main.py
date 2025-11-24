5e51eb6a91a0065da32b15ced836f06e1b424597
dt4_mac <ihromushin.y177@gmail.com>
2025-11-24 12:35:14 +0300

    basic class things

---
+++ Main.py
@@ -0,0 +1,11 @@
+from datetime import *
+from aiogram import Bot, Dispatcher
+from aiogram.types import Message
+from aiogram.filters.command import Command
+from aiogram.utils.keyboard import ReplyKeyboardBuilder, ReplyKeyboardMarkup, KeyboardButton
+from aiogram import F
+from aiogram.fsm.state import State, StatesGroup
+from aiogram.fsm.context import FSMContext
+
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton, WebAppInfo
+from aiogram.types import CallbackQuery
